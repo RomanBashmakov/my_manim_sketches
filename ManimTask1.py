05ef1889de038444c283c7823c9a51688c77c7aa
RomanBashmakov <bashmakov_r@inbox.ru>
2024-05-19 16:01:29 +0300

    frame Arut_Task done

--- ManimTask1.py
+++ ManimTask1.py
@@ -32,38 +32,72 @@ class ToyExample(Scene):
 class Zadacha_Ot_Aruta(Scene):
     def construct(self):
 
-        text_Task = Text("Задача от ", font_size=96)
-        # self.add(text_Task)
-
-        text_Arut = Text("Арута", font_size=96)
+        text_Task = Text("Задача", font_size=80)
+        text_Arut = Text("от инвестора Арута", font_size=80)
         for letter in text_Arut:
             letter.set_color(random_bright_color())
+        gTaskArut = VGroup(text_Task, text_Arut).arrange(direction = DOWN)
 
-        group = VGroup(text_Task, text_Arut).arrange(RIGHT)
-        # group.next_to(ORIGIN)
-        self.play(Create(group))
-        # self.wait(1)
-        self.play(group.animate(run_time=1, lag_ratio=0.1).shift(UP * 2))
+        self.play(gTaskArut.animate(run_time=1, lag_ratio=0.1).shift(UP * 5))
         self.wait(0.5)
 
-        # text_Bylo = Text("Было", font_size=96)
-        # text_Bylo = MarkupText('<span foreground="blue" size="x-large">Blue text</span> is <i>cool</i>!"')
-        text_Bylo = Text('Было 100$',t2c={'100':DARK_BLUE}, font_size=96)
-        # self.play(Create(text_Bylo))
-        # self.wait(1)
-
-        text_Stalo = Text('Стало 5000$',t2c={'5000':RED}, font_size=96)
-        # self.play(Create(text_Stalo))
-        # self.wait(1)
-
-        g = VGroup(text_Bylo, text_Stalo).arrange(direction = DOWN, aligned_edge = LEFT).scale(0.7).next_to(group,DOWN).shift(LEFT * 2)
+        text_Bylo = Text('Было a = 100$',t2c={'a = 100':DARK_BLUE}, font_size=96)
+        text_Stalo = Text('Стало A = 5000$',t2c={'A = 5000':RED}, font_size=96)
+        text_Vremya = Text('За время T = 20 месяцев',t2c={'20 месяцев':DARK_BROWN}, font_size=96)
+        g = VGroup(text_Bylo, text_Stalo, text_Vremya).scale(0.5).arrange(direction = DOWN, aligned_edge = LEFT).next_to(gTaskArut,DOWN * 5).shift(LEFT * 2)
 
         self.play(Create(g))
 
-        # self.add(text_Bylo, text_Stalo)
+        framebox1 = SurroundingRectangle(text_Bylo[0:13], buff = .1)
+        framebox2 = SurroundingRectangle(text_Stalo[0:15], buff = .1)
+        framebox3 = SurroundingRectangle(text_Vremya[0:23], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        # self.wait(0.2)
+        self.play(ReplacementTransform(framebox1,framebox2))
+        # self.wait(0.2)
+        self.remove(framebox1)
+        self.play(ReplacementTransform(framebox2,framebox3))
+        # self.wait(0.2)
+        self.remove(framebox3)
+        self.wait(0.2)
 
-        # self.play(Create(text_Bylo))
-        # self.play(Create(text_Stalo))
+        text_Question = Text("Вопрос:", font_size=80)
+        self.play(ReplacementTransform(g, text_Question))
+        self.wait(0.2)
 
-        self.wait(1)
+        text_Question2 = Text("Вопрос:", font_size=80).next_to(gTaskArut, DOWN * 2)
+        self.play(FadeOut(gTaskArut))
+        self.play(ReplacementTransform(text_Question, text_Question2))
+        self.wait(0.2)
+        framebox4 = SurroundingRectangle(text_Question2, buff = .1)
+        self.play(Create(framebox4))
+        self.wait(0.2)
+        self.remove(framebox4)
+        self.wait(0.2)
 
+        text_Question3_1 = Text("Помогите")
+        text_Question3_1_1 = Text("инвестору Аруту")
+        for letter in text_Question3_1_1:
+            letter.set_color(random_bright_color())
+        text_Question3_2 = Text("найти доходность")
+        text_Question3_3 = Text("его портфеля")
+        text_Question3_4 = Text("?", font_size=96)
+
+        group_text_Question3 = VGroup(text_Question3_1, text_Question3_1_1, text_Question3_2, text_Question3_3, text_Question3_4).arrange(direction = DOWN)
+        # group_text_Question3 = VGroup(text_Question3_1, text_Question3_2, text_Question3_3)
+
+        # paragraph_Question3 = Paragraph(
+        #     "Помогите",
+        #     "Аруту",
+        #     alignment="center",  # Выравнивание по центру
+        #     font_size=80,
+        # )
+        self.play(Write(group_text_Question3))
+        self.wait(0.2)
+
+class Zadacha_Ot_Aruta_Solving(Scene):
+    def construct(self):
+
+        self.wait(0.2)
